f0d8a568286c892d88d7a9dc65c35e23592adc68
Akshitha-95 <akshithaanjali@gmail.com>
2021-01-11 14:47:25 +0530

    another change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+	int i;
+	for(i=0;i<=10;i++)
+	{
+		System.out.println(i);
+	}
     }
 }
